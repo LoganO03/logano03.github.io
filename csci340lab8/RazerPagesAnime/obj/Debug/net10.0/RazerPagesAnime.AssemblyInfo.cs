d50f533a3321354fbbcea8c2970fdf06f01047c5
LoganO03 <j.logan.oroark@gmail.com>
2026-03-11 23:38:58 -0500

    Database functionality works

--- csci340lab8/RazerPagesAnime/obj/Debug/net10.0/RazerPagesAnime.AssemblyInfo.cs
+++ csci340lab8/RazerPagesAnime/obj/Debug/net10.0/RazerPagesAnime.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazerPagesAnime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22b022456ccd33981434d40836e2b9d5700855d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f4cb4bc81c76e601aa79c8ac52a1b792424e66c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazerPagesAnime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazerPagesAnime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

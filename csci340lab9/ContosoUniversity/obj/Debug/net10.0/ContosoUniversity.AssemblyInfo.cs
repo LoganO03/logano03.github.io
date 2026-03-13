9c1d536f0ef26e02674acc8fb5f81c429ed31f92
LoganO03 <j.logan.oroark@gmail.com>
2026-03-13 12:34:38 -0500

    Completed step 1 of tutorial

--- csci340lab9/ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
+++ csci340lab9/ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22b022456ccd33981434d40836e2b9d5700855d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfb251c2df7adf732a9af80c370838ba11d841a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

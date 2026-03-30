6923686ae7bb3b4be98782b8981b3d43749df378
LoganO03 <j.logan.oroark@gmail.com>
2026-03-30 03:08:59 -0500

    Up until The Instructor Entity Step 5 Lab 9

--- csci340lab9/ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
+++ csci340lab9/ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7a1cc34af319cc5882c335cfe1abc9993b046a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b512cfb053325a7e4aeb0b9840c7f45aa42ad8f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

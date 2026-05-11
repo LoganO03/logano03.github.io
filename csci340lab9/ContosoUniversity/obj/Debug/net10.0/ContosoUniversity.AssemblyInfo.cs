127de094668f40a4a91bfaf0a9059cdf86c2d363
LoganO03 <j.logan.oroark@gmail.com>
2026-05-11 10:16:09 -0500

    age sort now works

--- csci340lab9/ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
+++ csci340lab9/ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+925ad480911afdbd29adc076574ad14495e7f544")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82e2182fde3567d0598183f8eb4d7e684567378a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
